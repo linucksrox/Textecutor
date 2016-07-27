ab8eb46d1c1e18f4da2e33fbbdc98e2db2425ecf
Eric Daly <edaly@hpsnet.com>
2016-07-27 12:36:54 -0400

    Request SMS permission to allow MySMSReceiver to work

--- app/src/main/java/com/dalydays/android/fullvolumetext/MainActivity.java
+++ app/src/main/java/com/dalydays/android/fullvolumetext/MainActivity.java
@@ -1,22 +1,23 @@
 package com.dalydays.android.fullvolumetext;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView smsMessage;
+    public static final int MY_RECEIVE_SMS_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        smsMessage = (TextView) findViewById(R.id.sms_message_text_view);
-
-
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECEIVE_SMS}, MY_RECEIVE_SMS_REQUEST_CODE);
+        }
     }
-
-
 }
