f64c2fe598826a39cd0e7e7238ac5427f585cc68
linucksrox <ericdalyblog@dalydays.com>
2017-05-01 07:18:03 -0400

    Add TODO items outlining functionality

--- app/src/main/java/com/dalydays/android/fullvolumetext/MainActivity.java
+++ app/src/main/java/com/dalydays/android/fullvolumetext/MainActivity.java
@@ -24,3 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+// TODO Allow the user to choose from a list of contacts from the contacts content provider, so that only the contacts chosen are allowed to send text commands
+// TODO Allow the user to change the text string that initiates the full volume command
+// TODO Allow the user to add different types of commands, with different text strings, and tied to different sets of authorized users (or a global list)
+// TODO Wizard for implementing common use cases like full volume for trusted contacts
+// TODO Help within the app explaining how to do things, as interactive as possible (or the app should be as intuitive as possible, so documentation is not required)
+// TODO Implement a SQLite database to store preferences, authorized contacts, blocked numbers, etc.
