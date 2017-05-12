1e576519bbb1ea6e0de916c37f4981a107cec20d
Eric Daly <ericdalyblog@dalydays.com>
2017-05-12 13:10:16 -0400

    Add contact picker button and display results in a TextView

--- app/src/main/java/com/dalydays/android/fullvolumetext/MainActivity.java
+++ app/src/main/java/com/dalydays/android/fullvolumetext/MainActivity.java
@@ -1,15 +1,27 @@
 package com.dalydays.android.fullvolumetext;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int MY_RECEIVE_SMS_REQUEST_CODE = 1;
+    static final String LOG_TAG = MainActivity.class.getSimpleName();
+    static final int MY_RECEIVE_SMS_REQUEST_CODE = 1;
+    static final int PICK_CONTACT = 1;
+    Button pickContactButton;
+    TextView contactInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +33,59 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECEIVE_SMS}, MY_RECEIVE_SMS_REQUEST_CODE);
         }
 
+        pickContactButton = (Button) findViewById(R.id.btn_pick_contact);
+        pickContactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(intent, PICK_CONTACT);
+            }
+        });
 
+        contactInfo = (TextView) findViewById(R.id.tv_contact_info);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // check whether the result is ok
+        if (resultCode == RESULT_OK) {
+            // Check for the request code, we might be using multiple startActivityForResult
+            switch (requestCode) {
+                case PICK_CONTACT:
+                    contactPicked(data);
+                    break;
+            }
+        } else {
+            Log.e(LOG_TAG, "Failed to pick contact");
+        }
+    }
+
+    /**
+     * Query the Uri and read contact details. Handle the picked contact data.
+     * @param data
+     */
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+        try {
+            String phoneNo = null ;
+            String name = null;
+            // getData() method will have the Content Uri of the selected contact
+            Uri uri = data.getData();
+            //Query the content uri
+            cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+            // column index of the phone number
+            int  phoneIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            // column index of the contact name
+            int  nameIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            phoneNo = cursor.getString(phoneIndex);
+            name = cursor.getString(nameIndex);
+            Log.d(LOG_TAG, "Contact name/phone number: " + name + "/" + phoneNo);
+            // Set the value to the TextView
+            contactInfo.setText(name + " / " + phoneNo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
