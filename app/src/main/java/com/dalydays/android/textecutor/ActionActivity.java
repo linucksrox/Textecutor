4959084423c588f297eb7306884683caf4159f8e
linucksrox <ericdalyblog@dalydays.com>
2017-06-22 07:21:27 -0400

    Start setting up Action activity fragment

--- app/src/main/java/com/dalydays/android/textecutor/ActionActivity.java
+++ app/src/main/java/com/dalydays/android/textecutor/ActionActivity.java
@@ -1,8 +1,23 @@
 package com.dalydays.android.textecutor;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.widget.ListView;
+
 /**
  * Created by edaly on 6/19/17.
  */
 
-public class ActionActivity {
+public class ActionActivity extends FragmentActivity {
+
+    private ListView mActionList;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action);
+
+        mActionList = (ListView) findViewById(R.id.action_list);
+    }
 }
